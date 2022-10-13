f01f0e2e8a79c3a35e950fd1a04063f42143ac98
Pedro Henrique Sudario da Silva <84980739+petersudario@users.noreply.github.com>
2022-10-12 21:48:56 -0300

    v1.2.4

---
+++ projeto/result.php
@@ -0,0 +1,23 @@
+<?php  
+    $bom = "Bom"; 
+    $ruim = "Ruim"; 
+                if ($bpm > 78) {
+                    $result = false;
+                } 
+                else if ($bpm < 73)
+                {
+                    $result = false;
+                }
+                else if ($bpm >=74){
+                    $result = true;
+                }
+
+                if ($oxig >= 95){
+                    $result2 = true;
+                }
+                
+                if ($oxig < 95) {
+                    $result2 = false;
+                }
+
+            ?>
